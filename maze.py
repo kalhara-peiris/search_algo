0040d1fa29008d237aed7a528f49802a038e2649
Kalhara Peiris <kalhara.20220845@iit.ac.lk>
2025-03-13 22:11:26 +0530

    modifyng the maze

--- maze.py
+++ maze.py
@@ -1,11 +1,11 @@
 import random
 maze=[
-     [1,7,13,0,0,0],
-     [2,8,0,0,0,0],
-     [3,8,0,0,0,0],
-     [4,10,0,0,0,0],
-     [5,11,0,0,0,0],
-     [6,12,0,0,0,0],
+     ["1","7",13,0,0,0],
+     ["2","8",0,0,0,0],
+     ["3","9",0,0,0,0],
+     ["4","10",0,0,0,0],
+     ["5","12",0,0,0,0],
+     ["6","13",0,0,0,0],
      ]
 
 
@@ -17,12 +17,12 @@ while True:
     continue
   else:
     break
-maze[row][col]=1
+maze[row][col]="S"
 
 #goal node
 row_goal=random.randint(0,5)
 col_goal=random.randint(4,5)
-maze[row_goal][col_goal]=3
+maze[row_goal][col_goal]="G"
 
 count=0
 #Barriers
@@ -32,7 +32,7 @@ while True:
   row_barrier=random.randint(0,5)
   col_barrier=random.randint(0,5)
   if maze[row_barrier][col_barrier]==0:
-    maze[row_barrier][col_barrier]=2
+    maze[row_barrier][col_barrier]="B"
     count+=1
   else:
     continue
